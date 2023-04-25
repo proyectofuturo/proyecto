73c93080c1a0f83e7367cadbe9d364d3ade61174
proyectofuturo <131413606+proyectofuturo@users.noreply.github.com>
2023-04-25 09:35:53 -0600

    Add files via upload

---
+++ LN/LN/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace LN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLN = new System.Windows.Forms.TextBox();
+            this.txtx0 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtx00 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.datapapapa = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.datapapapa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LN";
+            // 
+            // txtLN
+            // 
+            this.txtLN.Location = new System.Drawing.Point(77, 79);
+            this.txtLN.Name = "txtLN";
+            this.txtLN.Size = new System.Drawing.Size(40, 20);
+            this.txtLN.TabIndex = 1;
+            this.txtLN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLN_KeyPress);
+            // 
+            // txtx0
+            // 
+            this.txtx0.Location = new System.Drawing.Point(164, 79);
+            this.txtx0.Name = "txtx0";
+            this.txtx0.Size = new System.Drawing.Size(40, 20);
+            this.txtx0.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x0";
+            // 
+            // txtx00
+            // 
+            this.txtx00.Location = new System.Drawing.Point(257, 79);
+            this.txtx00.Name = "txtx00";
+            this.txtx00.Size = new System.Drawing.Size(40, 20);
+            this.txtx00.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x1";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(77, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // datapapapa
+            // 
+            this.datapapapa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datapapapa.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.datapapapa.Location = new System.Drawing.Point(23, 167);
+            this.datapapapa.Name = "datapapapa";
+            this.datapapapa.Size = new System.Drawing.Size(851, 265);
+            this.datapapapa.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Pasos";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "x0";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "X";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "x1";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "F(x0)";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "f(x)";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "f(x1)";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "ET";
+            this.Column8.Name = "Column8";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(520, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "VALOR REAL";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(222, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(600, 121);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 444);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.datapapapa);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtx00);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtx0);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLN);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lineal";
+            ((System.ComponentModel.ISupportInitialize)(this.datapapapa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLN;
+        private System.Windows.Forms.TextBox txtx0;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtx00;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView datapapapa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtvalor;
+    }
+}
+
---
+++ LN/LN/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 1;
+          double  x, x0, x1, fx0, fx1, fx, ea, xant;
+        int ban = 0, ban2 = 0;
+
+        private void txtLN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                while ((ban + ban2) < 2)
+                {
+                    if (i == 1)
+                    {
+                        x = Convert.ToDouble(txtLN.Text);
+                        x0 = Convert.ToDouble(txtx0.Text);
+                        x1 = Convert.ToDouble(txtx00.Text);
+
+                        i++;
+
+                    }
+                    else
+                    {
+                        
+                        fx0 = Math.Log(x0);
+                        fx1 = Math.Log(x1);
+                        xant = fx; 
+                        fx = fx0 + ((fx1 - fx0) / (x1 - x0)) * (x - x0);
+
+                        ea = Math.Abs(((fx - xant) / fx) * 100);
+                        txtvalor.Text = Convert.ToString(fx);
+                        datapapapa.Rows.Add(i - 1, x, x0, x1, fx0, fx1, fx, ea);
+                        i++;
+                        x1 --;
+                        x0++;
+
+
+                        if (x0 == x)
+                        {
+                            ban = 1;
+                            x0--;
+                        }
+
+                        if (x1 == x)
+                        {
+                            ban2 = 1;
+                            x1++;
+                        }
+                    }
+
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        public void Limpiar()
+        {
+            txtLN.Text = "";
+            txtvalor.Text = "";
+            txtx0.Text = "";
+            txtx00.Text = "";
+            datapapapa.Rows.Clear();
+            i = 1;
+            x = 0;
+            x0 = 0;
+            x1 = 0;
+            fx = 0;
+            fx0 = 0;
+            fx1 = 0;
+            xant = 0;
+
+        }
+    }
+
+}
